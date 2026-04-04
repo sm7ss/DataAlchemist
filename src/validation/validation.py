73a3eb677eff8a8da1a3b2e38c6320b46dce1c08
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-04-04 00:50:41 -0100

    The schema was removed since it is not used in validation

--- src/validation/validation.py
+++ src/validation/validation.py
@@ -33,19 +33,96 @@ class validation(BaseModel):
         analysis= self.eda.basic_analysis_data
         
         if path.suffix == '.csv': 
-            frame= pl.scan_csv(path, n_rows=1000)
-            schema= frame.collect_schema()
+            frame= pl.read_csv(path, n_rows=1000)
         elif path.suffix == '.parquet': 
-            frame= pl.scan_parquet(path, n_rows=1000)
-            schema= frame.collect_schema()
+            frame= pl.read_parquet(path, n_rows=1000)
         else: 
             frame= pl.read_json(path, n_rows=1000)
-            schema= frame.schema
         
-        num_columns= frame.select(pl.selectors.numeric())
-        cat_columns= frame.select(pl.selectors.string())
+        num_columns= frame.select(pl.selectors.numeric()).columns
+        cat_columns= frame.select(pl.selectors.string()).columns
+        all_columns= frame.columns
         
-        
+        for analysis_data in analysis: 
+            columns= analysis[analysis_data]['columns']
+            enable= analysis[analysis_data]['enable']
+            
+            if not columns: 
+                if (analysis_data == 'distribution') and (enable == True): 
+                    if len(all_columns) < 1: 
+                        logger.warning(f'There are no columns available, {analysis_data} will be desactivated')
+                        self.eda.basic_analysis_data[analysis_data]['enable']= False
+                    else: 
+                        logger.info(f'{analysis_data} analysis will change their columns to be all columns, cause there are no columns available and the analysis is activated')
+                        self.eda.basic_analysis_data[analysis_data]['columns']= all_columns
+                elif (analysis_data == 'outliers') and (enable == True): 
+                    if len(num_columns) < 1: 
+                        logger.warning(f'There are no numric columns available, {analysis_data} will be desactivated')
+                        self.eda.basic_analysis_data[analysis_data]['enable']= False
+                    else: 
+                        logger.info(f'{analysis_data} analysis will change their columns to be all numeric columns, cause there are no columns available and the analysis is activated')
+                        self.eda.basic_analysis_data[analysis_data]['columns']= num_columns
+                elif (analysis_data == 'correlation') and (enable == True): 
+                    if len(num_columns) < 1: 
+                        logger.warning(f'There are no numeric columns available, {analysis_data} will be desactivated')
+                        self.eda.basic_analysis_data[analysis_data]['enable']= False
+                    else: 
+                        if len(num_columns) < 2: 
+                            logger.warning(f'{analysis_data} analysis cannot have less than 2 numeric columns. This analysis will be desactivated')
+                            self.eda.basic_analysis_data[analysis_data]['enable']= False
+                        else: 
+                            logger.info(f'{analysis_data} analysis will change their columns to be all numeric columns, cause there are no columns available and the analysis is activated')
+                            self.eda.basic_analysis_data[analysis_data]['columns']= num_columns
+                elif (analysis_data == 'category_dominance') and (enable == True): 
+                    if len(cat_columns) < 1: 
+                        logger.warning(f'There are no categoric columns available, {analysis_data} will be desactivated')
+                        self.eda.basic_analysis_data[analysis_data]['enable']= False
+                    else: 
+                        logger.info(f'{analysis_data} analysis will change their columns to be all categorical columns, cause there are no columns available and the analysis is activated')
+                        self.eda.basic_analysis_data[analysis_data]['columns']= cat_columns 
+                else: 
+                    continue
+            elif isinstance(columns, list): 
+                for col in columns: 
+                    if (analysis_data == 'distribution') and (enable == True): 
+                        if col not in all_columns: 
+                            logger.error(f'The column {col} doesnt exist on the frame.\nThe available columns are: {all_columns}')
+                    elif (analysis_data == 'outliers') and (enable == True): 
+                        if col not in num_columns: 
+                            logger.error(f'Th column {col} should be a numerical column.\nNumerical columns available: {num_columns}')
+                            raise ValueError(f'Th column {col} should be a numerical column.\nNumerical columns available: {num_columns}')
+                    elif (analysis_data == 'correlation') and (enable == True): 
+                        if col not in num_columns: 
+                            logger.error(f'The column {col} should be a numerical column.\nNumerical columns available: {num_columns}')
+                            raise ValueError(f'The column {col} should be a numerical column.\nNumerical columns available: {num_columns}')
+                    elif (analysis_data == 'category_dominance') and (enable == True): 
+                        if col not in cat_columns: 
+                            logger.error(f'The column {col} is not a categorical column.\nCateorical columns available: {cat_columns}')
+                            raise ValueError(f'The column {col} is not a categorical column.\nCateorical columns available: {cat_columns}')
+                    else: 
+                        continue
+            elif isinstance(columns, str): 
+                if (analysis_data == 'distribution') and (enable == True): 
+                    if columns not in all_columns: 
+                        logger.error(f'The column {columns} doesnt exist on the frame.\nThe available columns are: {all_columns}')
+                        raise ValueError(f'The column {columns} doesnt exist on the frame.\nThe available columns are: {all_columns}')
+                elif (analysis_data == 'outliers') and (enable == True): 
+                    if columns not in num_columns: 
+                        logger.error(f'Th column {columns} should be a numerical column.\nNumerical columns available: {num_columns}')
+                        raise ValueError(f'Th column {columns} should be a numerical column.\nNumerical columns available: {num_columns}')
+                elif (analysis_data == 'correlation') and (enable == True): 
+                    if columns not in num_columns: 
+                        logger.error(f'The column {columns} should be a numerical column.\nNumerical columns available: {num_columns}')
+                        raise ValueError(f'The column {columns} should be a numerical column.\nNumerical columns available: {num_columns}')
+                elif (analysis_data == 'category_dominance') and (enable == True): 
+                    if columns not in cat_columns: 
+                        logger.error(f'The column {columns} is not a categorical column.\nCateorical columns available: {cat_columns}')
+                        raise ValueError(f'The column {columns} is not a categorical column.\nCateorical columns available: {cat_columns}')
+                else: 
+                    continue
+            else: 
+                logger.error('The columns should be on a list or should be a string')
+                raise ValueError('The columns should be on a list or should be a string')
         
         return self
 
