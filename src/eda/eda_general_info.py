30eff268ee770b678e83ffb2369890f08de8fec7
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-04-06 00:33:05 -0100

    first impression to form the eda_general_info

---
+++ src/eda/eda_general_info.py
@@ -0,0 +1,36 @@
+from typing import List, Tuple
+
+import polars as pl 
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s-&(levelname)s-%(message)s')
+logger= logging.getLogger(__name__)
+
+class EdaGeneralInfo: 
+    def __init__(self, frame: pl.DataFrame):
+        self.frame= frame
+    
+    def frame_head(self) -> pl.DataFrame: 
+        return self.frame.head()
+    
+    def shape(self) -> Tuple[int]: 
+        return self.frame.shape
+    
+    def available_columns(self) -> List[str]: 
+        return self.frame.columns
+    
+    def basic_statistics(self) -> pl.DataFrame: 
+        return self.frame.describe()
+    
+    def eda_general_info_format(self) -> str: 
+        print('FRAME HEAD')
+        print(self.frame_head())
+        print('SHAPE')
+        print(self.shape())
+        print('AVAILABLE COLUMNS')
+        print(self.available_columns())
+        print('BASIC STATISTICS')
+        print(self.basic_statistics())
+
+
+
