0af1cd55cc0c84c6432cbcabe1c4d70c8dfb84ca
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-04-08 03:26:56 -0100

    general information about the EDA is ready, for now, the dictionary has already been included, validations were included for cases where there are no numerical or categorical columns or both, it also only keeps the metadata and that's it 🐥

--- src/eda/eda_general_info.py
+++ src/eda/eda_general_info.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict, Any, Optional
 
 import polars as pl 
 import logging
@@ -18,20 +18,61 @@ class EdaGeneralInfo:
     def available_columns(self) -> List[str]: 
         return self.frame.columns
     
-    def basic_statistics(self) -> pl.DataFrame: 
-        return self.frame.describe()
+    def basic_statistics(self) -> Optional[Dict[str, Any]]: 
+        statistics_dict= {}
+        numeric_frame= self.frame.select(pl.selectors.numeric())
+        if numeric_frame.is_empty(): 
+            return None
+        
+        describe= numeric_frame.describe()
+        
+        for col in describe.columns: 
+            if col != 'statistic': 
+                statistics_dict[col]={}
+                statistics_dict[col]['mean']= describe.filter(pl.col('statistic')=='mean')[col].item()
+                statistics_dict[col]['std']= describe.filter(pl.col('statistic')=='std')[col].item()
+                statistics_dict[col]['25']= describe.filter(pl.col('statistic')=='25%')[col].item()
+                statistics_dict[col]['50']= describe.filter(pl.col('statistic')=='50%')[col].item()
+                statistics_dict[col]['75']= describe.filter(pl.col('statistic')=='75%')[col].item()
+                statistics_dict[col]['min']= describe.filter(pl.col('statistic')=='min')[col].item()
+                statistics_dict[col]['max']= describe.filter(pl.col('statistic')=='max')[col].item()
+        
+        return statistics_dict
     
-    def eda_general_info_format(self) -> str:
-        print('SHAPE')
-        print(self.shape())
-        print('AVAILABLE COLUMNS')
-        print(self.available_columns())
-        print('BASIC STATISTICS')
-        print(self.basic_statistics())
+    def datatype(self) -> Dict[str, Any]: 
+        datatype_dict= {}
         
+        for col, type_col in self.frame.schema.items(): 
+            datatype_dict[col]= type_col
         
+        return datatype_dict
+    
+    def unique_values(self) -> Dict[str, Any]: 
+        unique_values_dict= {}
+        cat_vals= self.frame.select(pl.selectors.string())
+        if cat_vals.is_empty(): 
+            return None
         
+        for col in cat_vals.columns: 
+            unique_num= cat_vals[col].n_unique()
+            
+            unique_values_dict[col]=unique_num
         
+        return unique_values_dict
+    
+    def eda_general_info_format(self) -> Dict[str, Any]:
+        shape= self.shape()
+        
+        self.info_eda_dict['total_columns']= shape[1]
+        self.info_eda_dict['total_rows']= shape[0]
+        
+        self.info_eda_dict['available_columns']= self.available_columns()
+        self.info_eda_dict['datatype']= self.datatype()
+        
+        self.info_eda_dict['statistics']= self.basic_statistics()
+        self.info_eda_dict['unique_values']= self.unique_values()
+        
+        return self.info_eda_dict
 
 
 
