498dbe0a25e90be6b95b0253df4567ff611feb3c
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-04-15 03:37:45 -0100

    A new folder was created to handle input and output such as JSON files and soon txt, this also compiles and creates the files correctly

---
+++ src/io/folder_file_manager.py
@@ -0,0 +1,44 @@
+from typing import Dict, Any
+from pathlib import Path
+from datetime import datetime
+
+import json
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s-&(levelname)s-%(message)s')
+logger= logging.getLogger(__name__)
+
+class FolderAndFile: 
+    def __init__(self):
+        self.main_path= Path(__file__).resolve().parent.parent.parent
+    
+    def folder_analysis(self) -> Path: 
+        folder= 'eda_analysis'
+        path= self.main_path / folder
+        path.mkdir(parents=True, exist_ok=True)
+        return path
+    
+    def folder_date(self) -> Path: 
+        now= datetime.now().strftime('%Y-%m-%d')
+        folder= f'analysis_{now}'
+        path= self.folder_analysis() / folder
+        path.mkdir(parents=True, exist_ok=True)
+        return path
+    
+    def create_json(self, json_dict: Dict[str, Any]) -> Path: 
+        json_name= 'JSON_analysis.json'
+        file_path= self.folder_date() / json_name
+        
+        try: 
+            with open(file_path, 'w', encoding='utf-8') as f: 
+                json.dump(json_dict, f, indent=4, ensure_ascii=False)
+                logger.info(f'Json report was written succesfully')
+            return file_path
+        except json.JSONDecodeError: 
+            logger.error(f'The JSON file "{file_path.name}" is corrputed')
+            raise ValueError(f'The JSON file "{file_path.name}" is corrputed')
+        except Exception as e: 
+            logger.error(f'An error ocurred: {e}')
+            raise ValueError(f'An error ocurred: {e}')
+
+
