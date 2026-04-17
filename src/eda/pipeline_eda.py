9cfb316149d20763baf38496efb483e6a65a6bea
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-04-17 04:18:49 -0100

    first part of text for the general eda report is done

--- src/eda/pipeline_eda.py
+++ src/eda/pipeline_eda.py
@@ -13,6 +13,65 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(levelname)s-%(asctime)s-%(message)s')
 logger= logging.getLogger(__name__)
 
+class InfoGeneralEdaReport: 
+    def __init__(self, eda_general_dict: Dict[str, Any]):
+        self.dict= eda_general_dict
+    
+    def available_columns(self) -> str: 
+        available_columns= self.dict['general_eda']['available_columns']
+        return ', '.join(map(str, available_columns))
+    
+    def datatype_unique(self, type: Dict[str, Any]) -> str: 
+        text= ''
+        
+        for key, val in type.items(): 
+            text+= f'    - {key}: {val}\n'
+        
+        return text
+    
+    def statistics(self) -> str: 
+        statistics= self.dict['general_eda']['statistics']
+        text= ''
+        
+        for col in statistics: 
+            values= statistics[col]
+            mean= values['mean']
+            median= values['50']
+            std= values['std']
+            min= values['min']
+            max= values['max']
+            text+= f'''    
+        {col} 
+            - Mean: {mean} 
+            - Median: {median}
+            - Std: {std}
+            - Min:  {min}
+            - Max: {max}
+        '''
+        
+        return text
+    
+    def get_text(self) -> str: 
+        datatype= self.dict['general_eda']['datatype']
+        unique= self.dict['general_eda']['unique_values']
+        return f'''
+GENERAL INFO
+-----------------------------------------------------------------------
+Available Columns: {self.available_columns()}
+
+Column Type: 
+{self.datatype_unique(type=datatype)}
+Numeric Statistics: {self.statistics()} 
+Unique Categoric Values: 
+{self.datatype_unique(type=unique)} 
+'''
+
+class InfoNullEda: 
+    pass
+
+class InfoAnalysisEda: 
+    pass
+
 class EdaPipeline: 
     def __init__(self, config: BaseModel, config_var: BaseModel):
         frame= get_frame(file=config.path.data, overhead=config.path.overhead_percent)
@@ -36,26 +95,35 @@ class EdaPipeline:
     def eda_analysis_info(self) -> Dict[str, Any]: 
         return self.eda_analysis.analysis_data()
     
-    def pipeline_eda_json_file(self) -> Path: 
+    def pipeline_eda(self) -> Dict[str, Path]: 
         enable_general_eda= self.config_eda.general_information
         enable_null_analysis= self.config_eda.null_values
+        report= ''
         
         dict_eda={}
         
         if enable_general_eda: 
             general_eda= self.eda_general_info()
+            general_report= ''
             dict_eda['general_eda']= general_eda
         
         if enable_null_analysis: 
             null_analysis= self.eda_null_info()
+            null_report= ''
             dict_eda['null_analysis']= null_analysis
         
         analysis_data= self.eda_analysis_info()
+        if analysis_data: 
+            analysis_report= ''
         dict_eda['analysis_data']= analysis_data
         
-        path= FolderAndFile().create_json(json_dict=dict_eda)
+        json_path= FolderAndFile().create_json(json_dict=dict_eda)
+        txt_path= FolderAndFile().create_txt(report=report)
         
-        return path
+        return {
+            'JSON_path': json_path, 
+            'TXT_path': txt_path
+        }
 
 
 
