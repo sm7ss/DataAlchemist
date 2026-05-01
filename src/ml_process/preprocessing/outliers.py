8f3e6b1c114cb011178f95c79e99743c75507706
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-05-01 03:13:24 -0100

    the following points were added: - the iqr part was separated to iqr_method - iqr_auto_expr_method the iqr part was extracted - the manual part was added to the iqr method - the main configuration was added to know if the method will be automatic or not

--- src/ml_process/preprocessing/outliers.py
+++ src/ml_process/preprocessing/outliers.py
@@ -55,16 +55,17 @@ class FilterOutliersExpr:
         
         return expression
 
-# IMPLEMENT MANUAL
 class OutlierPreprocessingPipeline: 
-    def __init__(self, frame: pl.DataFrame, config_outlier: BaseModel):
+    def __init__(self, frame: pl.DataFrame, config_outlier: BaseModel, config: BaseModel):
         self.frame= frame.with_row_index()
-        self.config= config_outlier
+        
+        self.config= config
+        self.o_config= config_outlier
         
         self.numeric_frame= self.frame.select(pl.selectors.numeric())
         
         self.value= ImputeOutlierValue(frame=self.frame)
-        self.filter_expr= FilterOutliersExpr()
+        self.filter= FilterOutliersExpr()
     
     def input_expr(self, col: str, method: OutlierImpute, list_index_out: List[int]) -> pl.Expr: 
         value= self.value.get_value(col=col, method=method)
@@ -79,7 +80,7 @@ class OutlierPreprocessingPipeline:
         lower: Optional[Union[int, float]]= None
         ) -> pl.Expr: 
         
-        return self.filter_expr.get_expr(
+        return self.filter.get_expr(
             method=method, 
             col=col, 
             out_index=out_index, 
@@ -101,20 +102,33 @@ class OutlierPreprocessingPipeline:
         
         return expression
     
+    def iqr_method(self, col) -> Dict[str, Any]: 
+        q1= self.numeric_frame[col].quantile(0.25)
+        q3= self.numeric_frame[col].quantile(0.75)
+        
+        iqr= q3 - q1
+        
+        lower= q1 - iqr *1.5
+        upper= q3 + iqr *1.5
+        
+        outlier_frame= self.numeric_frame.filter((pl.col(col) < lower) | (pl.col(col) > upper))
+        list_index_out= outlier_frame.get_column('index').to_list()
+        
+        return {
+            'lower': lower, 
+            'upper': upper, 
+            'list': list_index_out
+        }
+    
     def iqr_auto_expr_method(self, outlier_dict: Dict[str, Any]) -> List[pl.Expr]:
         list_iqr_expr= []
         
         for col in self.numeric_frame.columns:
-            q1= self.numeric_frame[col].quantile(0.25)
-            q3= self.numeric_frame[col].quantile(0.75)
+            dict_iqr_method= self.iqr_method(col=col)
+            list_index_out= dict_iqr_method['list']
             
-            iqr= q3 - q1
-            
-            lower= q1 - iqr *1.5
-            upper= q3 + iqr *1.5
-            
-            outlier_frame= self.numeric_frame.filter((pl.col(col) < lower) | (pl.col(col) > upper))
-            list_index_out= outlier_frame.get_column('index').to_list()
+            lower= dict_iqr_method['lower']
+            upper= dict_iqr_method['upper']
             
             if list_index_out: 
                 logger.info(f'The total outliers detected were {len(list_index_out)} for column {col}')
@@ -123,49 +137,90 @@ class OutlierPreprocessingPipeline:
                 sugg= outlier_dict[col].get('suggestion')
                 
                 for type_op, operation in sugg.items(): 
-                    if percent > 0: 
-                        if percent < self.config.filter_percent: 
-                            if type_op == 'filter' and operation: 
-                                logger.info(f'Column {col} was filtered into type {type_op} with operation {operation}')
-                                expr= self.filter_expr(
-                                    method=operation, 
-                                    col=col, 
-                                    out_index=list_index_out, 
-                                    upper=upper, 
-                                    lower=lower
-                                )
-                                list_iqr_expr.append(expr)
-                            else: 
-                                logger.warning(f'Column {col} does not have available filter operation')
-                        elif percent < self.config.impute_percent: 
-                            if type_op == 'impute' and operation: 
-                                logger.info(f'Column {col} was imputed in type {type_op} with operation {operation}')
-                                expr= self.input_expr(
-                                    col=col, 
-                                    method=operation, 
-                                    list_index_out=list_index_out
-                                )
-                                list_iqr_expr.append(expr)
-                            else: 
-                                logger.warning(f'Column {col} does not have available impute operation')
-                        elif percent > self.config.transform_percent: 
-                            if type_op == 'transform' and operation: 
-                                logger.info(f'Column {col} was transformed into the type {type_op} with the transformer {operation}')
-                                expr= self.transform_expr(col=col, method=operation)
-                                list_iqr_expr.append(expr)
-                            else: 
-                                logger.warning(f'Column {col} does not have available transform operation')
-                        elif percent > self.config.flag_percent: 
-                            if type_op == 'flag' and True: 
-                                logger.info(f'Column {col} was flagged')
-                                expr= self.flag_expr(col=col, list_index_outlier=list_index_out)
-                                list_iqr_expr.append(expr)
-                            else: 
-                                logger.info(f'Column {col} was not flagged')
-                    else: 
-                        logger.info(f'Column {col} has 0% of outliers')
+                    if percent < self.o_config.filter_percent: 
+                        if type_op == 'filter' and operation: 
+                            logger.info(f'Column {col} was filtered into type {type_op} with operation {operation}')
+                            expr= self.filter_expr(
+                                method=operation, 
+                                col=col, 
+                                out_index=list_index_out, 
+                                upper=upper, 
+                                lower=lower
+                            )
+                            list_iqr_expr.append(expr)
+                            break
+                    elif percent < self.o_config.impute_percent: 
+                        if type_op == 'impute' and operation: 
+                            logger.info(f'Column {col} was imputed in type {type_op} with operation {operation}')
+                            expr= self.input_expr(
+                                col=col, 
+                                method=operation, 
+                                list_index_out=list_index_out
+                            )
+                            list_iqr_expr.append(expr)
+                            break
+                    elif percent > self.o_config.transform_percent: 
+                        if type_op == 'transform' and operation: 
+                            logger.info(f'Column {col} was transformed into the type {type_op} with the transformer {operation}')
+                            expr= self.transform_expr(col=col, method=operation)
+                            list_iqr_expr.append(expr)
+                            break
+                    elif percent > self.o_config.flag_percent: 
+                        if type_op == 'flag' and True: 
+                            logger.info(f'Column {col} was flagged')
+                            expr= self.flag_expr(col=col, list_index_outlier=list_index_out)
+                            list_iqr_expr.append(expr)
+                            break
+                        else: 
+                            logger.info(f'Column {col} was not flagged')
             else: 
                 logger.info(f'No outliers were detected for {col}')
+        
+        return list_iqr_expr
+    
+    def iqr_manual_expr_method(self) -> List[pl.Expr]: 
+        list_iqr_expr= []
+        
+        filter= self.config.ml_preprocessing.outlier.filter
+        i_outlier= self.config.ml_preprocessing.outlier.impute_outliers
+        flag= self.config.ml_preprocessing.outlier.flag
+        transform= self.config.ml_preprocessing.outlier.transform
+        
+        for col in self.numeric_frame.columns: 
+            dict_method= self.iqr_method(col=col)
+            
+            if filter: 
+                logger.info(f'Column {col} was filtered with operation {filter}')
+                expr= self.filter_expr(
+                    method=filter, 
+                    col=col, 
+                    out_index=dict_method['list'], 
+                    lower=dict_method['lower'], 
+                    upper=dict_method['upper']
+                )
+                list_iqr_expr.append(expr)
+            elif i_outlier: 
+                logger.info(f'Column {col} was imputed with operation {i_outlier}')
+                expr= self.input_expr(
+                    col=col, 
+                    method=i_outlier, 
+                    list_index_out=dict_method['list']
+                )
+                list_iqr_expr.append(expr)
+            elif transform: 
+                logger.info(f'Column {col} was transformed with the transformer {transform}')
+                expr= self.transform_expr(
+                    col=col, 
+                    method=transform
+                )
+                list_iqr_expr.append(expr)
+            elif flag == True: 
+                logger.info(f'Column {col} was flagged')
+                expr= self.flag_expr(
+                    col=col, 
+                    list_index_outlier=dict_method['list']
+                )
+                list_iqr_expr.append(expr)
         
         return list_iqr_expr
     
@@ -180,8 +235,7 @@ class OutlierPreprocessingPipeline:
                 if auto:
                     list_expr= self.iqr_auto_expr_method(outlier_dict=outlier_dict)
                 else: 
-                    logger.error('Manual preprocessing is not done yet')
-                    raise ValueError('Manual preprocessing is not done yet')
+                    list_expr= self.iqr_manual_expr_method()
         
         frame= self.frame.with_columns(list_expr)
         
