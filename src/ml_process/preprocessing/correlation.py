163ae667b57cd002167cbfc3eabcdf5fa9ab49b6
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-05-03 03:46:09 -0100

    the correlationPreprocessing class architecture was redone due to code inconsistencies

--- src/ml_process/preprocessing/correlation.py
+++ src/ml_process/preprocessing/correlation.py
@@ -9,30 +9,31 @@ logging.basicConfig(level=logging.INFO, format='%(levelname)s-%(asctime)s-%(mess
 logger= logging.getLogger(__name__)
 
 class CorrelationExpr: 
-    @staticmethod
-    def remove(frame: pl.DataFrame, col: Union[str, List[str]]) -> pl.DataFrame: 
-        return frame.drop(col)
+    def __init__(self, frame: pl.DataFrame):
+        self.frame= frame
     
-    @staticmethod
-    def join(col_1: str, col_2: str) -> pl.Expr: 
-        return (pl.col(col_1) + pl.col(col_2) / 2).alias(f'{col_1}_{col_2}_avg')
+    def remove(self, col: Union[str, List[str]]) -> pl.DataFrame: 
+        return self.frame.drop(col)
+    
+    def join(self, col_1: str, col_2: str) -> pl.Expr: 
+        return ((pl.col(col_1) + pl.col(col_2)) / 2).alias(f'{col_1}_{col_2}_avg')
 
 class CorrelationPreprocessing: 
     def __init__(self, frame: pl.DataFrame, corr_dict: Dict[str, Any], config: BaseModel):
         self.frame= frame
-        self.num_frame= frame.select(pl.selectors.numeric())
         
-        self.method= config.ml_preprocessing.correlation.high_correlation
-        self.column= config.ml_preprocessing.correlation.remove_column
+        self.corr_config= config.ml_preprocessing.correlation
         
         self.corr= corr_dict.get('high_correlations')
-        self.expr= CorrelationExpr()
+        self.expr= CorrelationExpr(frame=self.frame)
     
-    def auto_correlation(self) -> Union[str, List[pl.Expr]]: 
-        list_expr= []
+    def auto_correlation(self) -> Dict[List[pl.Expr], List[str]]: 
+        if not self.corr:
+            logger.info('No high correlation was found') 
+            return None
         
-        if not self.corr: 
-            return 'No high correlation was found'
+        list_expr= []
+        list_col_drop= []
         
         for i in range(len(self.corr)): 
             col_1= self.corr[i][0]
@@ -41,31 +42,45 @@ class CorrelationPreprocessing:
             logger.info(f'Column {col_1} and column {col_2} were joined')
             
             expr= self.expr.join(col_1=col_1, col_2=col_2)
-            
             list_expr.append(expr)
+            list_col_drop.append(col_1)
+            list_col_drop.append(col_2)
         
-        return list_expr
+        return {
+            'drop': list_col_drop, 
+            'expr': list_expr
+        }
     
-    def manual_correlation(self) -> Union[pl.DataFrame, List[pl.Expr]]: 
-        if not self.corr: 
-            return 'No high correlation was found'
+    def manual_correlation(self) -> Union[pl.DataFrame, List[pl.Expr], None]: 
+        if not self.corr:
+            logger.info('No high correlation was found') 
+            return None
+        
+        column = self.corr_config.remove_column
+        method= self.corr_config.high_correlation
         
         list_expr= []
+        list_drop= []
         
-        match self.method: 
-            case HighCorrelationActions.REMOVE: 
-                self.frame= self.expr.remove(frame=self.frame, col=self.column)
-                logger.info(f'Column {self.column} was deleated')
-            case HighCorrelationActions.JOIN: 
-                for i in range(len(self.corr)): 
-                    col_1= self.corr[i][0]
-                    col_2= self.corr[i][1]
-                    expr= self.expr.join(col_1=col_1, col_2=col_2)
-                    list_expr.append(expr)
-                    list_expr.append(f'Column {col_1} and {col_2} were joined')
+        if method == HighCorrelationActions.REMOVE: 
+            self.frame= self.expr.remove(frame=self.frame, col=column)
+            logger.info(f'Column {self.column} was deleated')
+        elif method == HighCorrelationActions.JOIN: 
+            for i in range(len(self.corr)): 
+                col_1= self.corr[i][0]
+                col_2= self.corr[i][1]
+                expr= self.expr.join(col_1=col_1, col_2=col_2)
+                list_expr.append(expr)
+                list_drop.append(col_1)
+                list_drop.append(col_2)
+                
+                logger.info(f'Column {col_1} and {col_2} were joined')
         
         if list_expr: 
-            return list_expr
+            return {
+            'drop': list_drop, 
+            'expr': list_expr
+        }
         
         return self.frame
 
