8d5bf95198745c7603b6aef2cba8117794ea8026
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-05-05 03:22:38 -0100

    comments on strategies to complement were added

--- src/strategies/pre_processing_strategies.py
+++ src/strategies/pre_processing_strategies.py
@@ -1,28 +1,27 @@
 from enum import Enum
 
+# 🚨🚨🚨
 class Scaler(str, Enum): 
     ROBUSTSCALER= 'robustScaler'
     STANDARDSCALER= 'standarScaler'
     MINMAXSCALER= 'minMaxScaler'
 
+# 🚨🚨🚨
 class Encoder(str, Enum): 
     ORDINALENCODER= 'ordinalEncoder'
     TARGETENCODER= 'targetEncoder'
     ONEHOTENCODER= 'oneHotEncoder'
 
-# THIS IS DONE
 class NullNumericHandler(str, Enum): 
     FILTER= 'filter'
     MEDIAN= 'median'
     ZERO= 'zero'
     MEAN= 'mean'
 
-# THIS IS DONE
 class NullCategoricHandler(str, Enum): 
     FILTER= 'filter'
     CONSTANTVALUE= 'constantValue'
 
-# THIS IS DONE
 class DistributionTransformer(str, Enum): 
     LOG1P= 'log1p'
     SQRT= 'sqrt'
@@ -32,12 +31,10 @@ class OutlierFilter(str, Enum):
     TRIM= 'trim'
     CAPPING= 'capping'
 
-# THIS IS DONE
 class OutlierImpute(str, Enum): 
     MEDIAN= 'median'
     MEAN= 'mean'
 
-# THIS IS DONE
 class OutlierTransform(str, Enum): 
     LOG1P= 'log1p'
     SQRT= 'sqrt'
@@ -49,8 +46,8 @@ class CorrSampling(str, Enum):
 class HighCorrelationActions(str, Enum): 
     REMOVE= 'remove'
     JOIN= 'join'
-    FILTER= 'filter'
 
+# 🚨🚨🚨 
 class CategoryOperation(str, Enum): 
     GROUP= 'group'
 
