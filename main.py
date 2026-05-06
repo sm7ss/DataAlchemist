64c98a03884a62567ce1399afbda89523fb858ed
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-05-06 22:51:26 -0100

    quick execution

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from src.validation.read_validation import ReadConfig
 from src.eda.pipeline_eda import EdaPipeline
+from src.ml_process.preprocessing.pipeline import AutoPipeline
+from src.get_frame import get_frame
+
+import json
 
 config= ReadConfig().read_config()
 
 config_var= config['config_vars']
+config_preprocessing= config['preprocessing']
 config= config['config']
 
-EdaPipeline(config=config, config_var=config_var).pipeline_eda()
+frame= get_frame(file=config.path.data)
+
+dict_eda_files= EdaPipeline(frame=frame, config=config, config_var=config_var).pipeline_eda()
+json_path= dict_eda_files['JSON_path']
+
+with open(json_path, 'r', encoding='utf-8') as f: 
+    file= json.load(f)
+
+frame= frame.with_row_index()
+
+preprocessing= AutoPipeline(frame=frame, analysis=file, config=config, config_pre=config_preprocessing)
+
+new_frame= preprocessing.auto_frame_tests()
+
+
