f482e543e7fa6328c93f40005e3c4bdba4bc6194
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-05-09 00:40:54 -0100

    The name of the strategies were changed

--- src/strategies/strategies.py
+++ src/strategies/strategies.py
@@ -1,13 +1,13 @@
 from enum import Enum
 
-class analysis_outliers(str, Enum): 
+class AnalysisOutliers(str, Enum): 
     IQR = 'iqr'
 
-class category_dominance_tn(int, Enum):
+class CategoryDominance(int, Enum):
     MAX = 100
     MIN = 2
 
-class category_dominance_rtp(float, Enum): 
+class CategoryDominanceRtp(float, Enum): 
     MAX = 1
     MIN = 0.0
 
