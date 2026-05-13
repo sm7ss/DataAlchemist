d5c31f8f56ee404aa629fa7c181fc33de08d4b90
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-05-13 03:35:18 -0100

    validation of new fields in config and config_ml configuration

--- src/validation/ml_validation.py
+++ src/validation/ml_validation.py
@@ -1,4 +1,7 @@
-from pydantic import BaseModel, Field
+from ..strategies.strategies import AnalysisOutliers
+
+from typing import Optional
+from pydantic import BaseModel, Field, field_validator
 
 class ml_training_val(BaseModel): 
     target: str
@@ -9,5 +12,13 @@ class ml_training_val(BaseModel):
     
     encoder: None
     scaler: None
+    scaler_outlier_method: Optional[AnalysisOutliers]
+    
+    @field_validator('scaler_outlier_method')
+    def scaler_outlier_method_val(cls, v): 
+        if not v: 
+            return AnalysisOutliers.IQR
+        else: 
+            return v
 
 
--- src/validation/validation_modeling/validation.py
+++ src/validation/validation_modeling/validation.py
@@ -8,6 +8,15 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(levelname)s-%(asctime)s-%(mesagge)s')
 logger= logging.getLogger(__name__)
 
+class sacler_rules_val(BaseModel): 
+    robust_scaler_percent: float= Field(ge=0.01, le=1.0)
+    standard_scaler_percent: float= Field(ge=0.01, le=1.0)
+
+class best_model_rules: 
+    cv_fold: int= Field(ge=0, le=10)
+    train_test_difference_percent: float= Field(ge=0.01, le=1.00)
+    unstable_cross_validation_percent: float= Field(ge=0.01, le=1.00)
+
 class linear_regression_val(BaseModel): 
     enable: bool
     fit_intercept: List[bool]
@@ -102,5 +111,6 @@ class grid_search_cv_val(BaseModel):
         return n
 
 class modeling_val(BaseModel): 
+    scaler_rules: sacler_rules_val
     models_hyperparameters: models_hyperparameters_val
     grid_search_cv: grid_search_cv_val
