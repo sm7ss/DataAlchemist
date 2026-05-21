f7883cd982979bdd47668a6ba51b039698fba76b
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-05-21 04:14:39 -0100

    expressions (for operations) and removed from columns was added :D

---
+++ src/cleaning/nulls.py
@@ -0,0 +1,68 @@
+from ..strategies.cleaning_strategies import NumericNulls, CategoricNulls
+
+from typing import List
+from pydantic import BaseModel
+
+import polars as pl 
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s-%(asctime)s-%(message)s')
+logger= logging.getLogger(__name__)
+
+class CatNullExpr: 
+    def __init__(self, model: BaseModel):
+        self.null_cat = model.cleaning.null_values.null_impute_categorics
+    
+    def input_value(self, cat_col: str) -> pl.Expr: 
+        return pl.col(cat_col).fill_null(self.null_cat)
+    
+    def input_mode(self, cat_col: str) -> pl.Expr: 
+        return pl.col(cat_col).mode().first()
+    
+    def input_cat_col(self, cat_col: str) -> pl.Expr: 
+        match self.null_cat: 
+            case CategoricNulls.MODE: 
+                expr= self.input_mode(cat_col=cat_col)
+            case _: 
+                expr= self.input_value(cat_col=cat_col)
+        return expr
+
+class NumNullExpr: 
+    def __init__(self, model: BaseModel):
+        self.null_num = model.cleaning.null_values.null_impute_numerics
+    
+    def operation_fill(self, col_num: str) -> pl.Expr:
+        expr= (getattr(pl.col(col_num), self.null_num)())
+        return pl.col(col_num).fill_null(expr)
+    
+    def input_value(self, col_num: str) -> pl.Expr: 
+        return pl.col(col_num).fill_null(self.null_num)
+    
+    def input_num_data(self, col_num: str) -> pl.Expr: 
+        match self.null_num:
+            case NumericNulls.MEAN | NumericNulls.MEDIAN: 
+                expr= self.operation_fill(col_num=col_num)
+            case _:
+                expr= self.input_value(col_num=col_num)
+        return expr
+
+class DeleteData: 
+    @staticmethod
+    def delete_column(frame: pl.DataFrame, list_col: List[str]) -> pl.DataFrame: 
+        return frame.drop(list_col)
+    
+    @staticmethod
+    def delete_row_expr(frame: pl.DataFrame) -> pl.Expr: 
+        columna = frame.get_column('index').to_list()
+        return ~pl.col('index').is_in(columna)
+
+
+
+
+
+
+
+
+
+
+
