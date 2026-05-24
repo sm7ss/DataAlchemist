0e8fc288180a66a962b6fe017bdadf81d70fd11c
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-05-24 04:17:25 -0100

    second part added for the null pipeline, this in itself is not finished but the logic of decision is already in place

--- src/cleaning/nulls.py
+++ src/cleaning/nulls.py
@@ -1,6 +1,6 @@
 from ..strategies.cleaning_strategies import NumericNulls, CategoricNulls
 
-from typing import List
+from typing import List, Optional, Tuple, Dict, Any
 from pydantic import BaseModel
 
 import polars as pl 
@@ -55,6 +55,58 @@ class DeleteData:
     def delete_row_expr(frame: pl.DataFrame) -> pl.Expr: 
         columna = frame.get_column('index').to_list()
         return ~pl.col('index').is_in(columna)
+
+class NullAnalyseDelete: 
+    def __init__(self, frame: pl.DataFrame, JSON: Dict[str, Any], model: BaseModel, model_cleaning: BaseModel):
+        self.frame= frame
+        
+        self.JSON= JSON.get('null_analysis', None)
+        self.model= model
+        
+        self.cat= CatNullExpr(model=self.model)
+        self.num= NumNullExpr(model=model)
+        self.delete= DeleteData()
+    
+    def cat_impute(self, col: str) -> pl.Expr: 
+        return self.cat.input_cat_col(cat_col=col)
+    
+    def num_impute(self, col: str) -> pl.Expr: 
+        return self.num.input_num_data(col_num=col)
+    
+    def delete(self, col: str) -> pl.Expr: 
+        frame= self.frame.with_row_index()
+        
+        # AQUI AGREGAR LA LOGICA DE INDEX PARA ELIMINAR SEA COLUMNA O ROW 
+        # AGREGARELO A UNA LISTA O DICCIONARIO Y PASARLO A OBTAIN_NULL_ACTION
+        
+        
+        
+    
+    def obtain_null_actions(self) -> Optional[Tuple[List[str]]]: 
+        if not self.JSON: 
+            logger.info('No nulls were found')
+            return None
+        
+        delete_columns_or_row= []
+        analyse_columns= []
+        
+        for col in self.JSON: 
+            if col != 'total_nulls': 
+                action= self.JSON[col]['action']
+                
+                if action == 'delete': 
+                    logger.info(f'Column or row {col} will be deleated')
+                    delete_columns_or_row.append(col)
+                elif action == 'analyse': 
+                    logger.info(f'Column {col} will be analysed')
+                    analyse_columns.append(col)
+                else: 
+                    logger.info(f'Column {col} will be keeped')
+                    continue
+        
+        return (delete_columns_or_row, analyse_columns)
+
+
 
 
 
