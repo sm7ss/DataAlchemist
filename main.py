c8cc1731c4f3d1ade4d40aac18c3589bf8fc7fc7
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-05-28 03:41:55 -0100

    it runs the entire pipeline, from obtaining the frame to the feature engineering

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from src.validation.read_validation import ReadConfig
-from src.eda.pipeline_eda import EdaPipeline
-from src.ml_process.preprocessing.pipeline import AutoPipeline
 from src.get_frame import get_frame
+from src.eda.pipeline_eda import EdaPipeline
+from src.cleaning.pipeline import CleanDataFrame
+from src.ml_process.preprocessing.pipeline import AutoPipeline
 
 import json
 
@@ -10,11 +11,25 @@ config= ReadConfig().read_config()
 config_var= config['config_vars']
 config_preprocessing= config['preprocessing']
 config_modeling= config['modeling']
+config_cleaning= config['cleaning']
 config= config['config']
 
 frame= get_frame(file=config.path.data)
 
 dict_eda_files= EdaPipeline(frame=frame, config=config, config_var=config_var).pipeline_eda()
+json_path= dict_eda_files['JSON_path']
+
+with open(json_path, 'r', encoding='utf-8') as f: 
+    file= json.load(f)
+
+frame_cleaned= CleanDataFrame(
+    frame=frame, 
+    config=config, 
+    config_clean=config_cleaning, 
+    JSON=file
+).clean_dataframe()
+
+dict_eda_files= EdaPipeline(frame=frame_cleaned, config=config, config_var=config_var).pipeline_eda()
 json_path= dict_eda_files['JSON_path']
 
 with open(json_path, 'r', encoding='utf-8') as f: 
