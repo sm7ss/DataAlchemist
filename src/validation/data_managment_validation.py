bf1b689dc79349febc49e269f5ff77f6fd07238f
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-07-29 22:14:35 -0100

    The data handling fields from the main config were changed to another file

--- src/validation/data_managment_validation.py
+++ src/validation/data_managment_validation.py
@@ -1,7 +1,8 @@
-from pydantic import BaseModel, Field, model_validator
+from pydantic import BaseModel, Field, model_validator, field_validator
 from pathlib import Path
 import logging
 
+from typing import Optional
 from ..strategies.strategies import ProtectionData
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s-%(asctime)s-%(message)s')
@@ -9,7 +10,7 @@ logger= logging.getLogger(__name__)
 
 class clean_data_val(BaseModel): 
     enable: bool
-    name: str
+    name: Optional[str]
     
     @model_validator(mode='after')
     def name_val(self): 
@@ -22,7 +23,7 @@ class clean_data_val(BaseModel):
 
 class preprocessing_data_val(BaseModel): 
     enable: bool
-    name: str
+    name: Optional[str]
     
     @model_validator(mode='after')
     def name_val(self): 
@@ -35,17 +36,31 @@ class preprocessing_data_val(BaseModel):
 
 class output_val(BaseModel): 
     clean_data: clean_data_val
-    data_protection: preprocessing_data_val
+    preprocessing_data: preprocessing_data_val
 
 class data_protection_val(BaseModel): 
     enable: bool
     method: ProtectionData
     delete_original_after: bool
 
-class data_managment_val(BaseModel): 
+class data_manag_val(BaseModel): 
     output: output_val
     data_protection: data_protection_val
     
     overhead_percent: float= Field(ge=1.0, le=3.0)
     sample_data_percent: float= Field(gt=0.0, lt=1.0)
+
+class data_managment_val(BaseModel): 
+    path: str
+    data_management: data_manag_val
+    
+    @field_validator('path')
+    def path_val(cls, v): 
+        path= Path(__file__).parent.parent.parent / 'config' / 'data' / v
+        
+        if path.suffix not in ['.csv', '.parquet']: 
+            logger.error(f'File {v} should be a .csv or a .parquet')
+            raise ValueError(f'File {v} should be a .csv or a .parquet')
+        
+        return path
 
