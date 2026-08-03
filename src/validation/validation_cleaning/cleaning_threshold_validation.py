047f6659accb47ba7fb2d4e42132b7d28e3228d9
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-08-03 21:48:03 -0100

    name and enable the output now in threshold

--- src/validation/validation_cleaning/cleaning_threshold_validation.py
+++ src/validation/validation_cleaning/cleaning_threshold_validation.py
@@ -1,5 +1,26 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
+from typing import Optional
 
-class config_cleaning(BaseModel): 
-    rows_percent: int
-    columns_percent: int
+from datetime import datetime
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s-%(asctime)s-%(message)s')
+logger= logging.getLogger(__name__)
+
+class output_val(BaseModel): 
+    name: Optional[str]
+    enable: Optional[bool]
+    
+    @model_validator(mode='after')
+    def name_val(self): 
+        if self.enable:
+            date= datetime.now().strftime('%d-%m-%Y')
+            path= Path(__file__).parent.parent.parent/'data_output'/'cleaned'/date/self.name
+            self.name= path / self.name
+            logger.info('The path for the output of the cleaned file is in place')
+        
+        return self
+
+class cleaning_val(BaseModel): 
+    output: output_val
