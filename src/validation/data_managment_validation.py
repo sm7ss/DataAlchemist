7c715c7d121ab8820a8be5f366c3ccd88c7fe65b
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-08-03 21:50:02 -0100

    Data output handling was removed

--- src/validation/data_managment_validation.py
+++ src/validation/data_managment_validation.py
@@ -1,42 +1,11 @@
-from pydantic import BaseModel, Field, model_validator, field_validator
+from pydantic import BaseModel, Field, field_validator
 from pathlib import Path
 import logging
 
-from typing import Optional, List
 from ..strategies.strategies import ProtectionData
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s-%(asctime)s-%(message)s')
 logger= logging.getLogger(__name__)
-
-class clean_data_val(BaseModel): 
-    enable: bool
-    name: Optional[str]
-    
-    @model_validator(mode='after')
-    def name_val(self): 
-        if self.enable:
-            path= Path(__file__).parent.parent.parent / 'config' / 'data'
-            self.name= path / self.name
-            logger.info('The path for the output of the cleaning file is in place')
-        
-        return self
-
-class preprocessing_data_val(BaseModel): 
-    enable: bool
-    name: Optional[str]
-    
-    @model_validator(mode='after')
-    def name_val(self): 
-        if self.enable:
-            path= Path(__file__).parent.parent.parent / 'config' / 'data'
-            self.name= path / self.name
-            logger.info('The path for the output of the preprocessing file is in place')
-        
-        return self
-
-class output_val(BaseModel): 
-    clean_data: clean_data_val
-    preprocessing_data: preprocessing_data_val
 
 class data_protection_val(BaseModel): 
     enable: bool
@@ -44,16 +13,12 @@ class data_protection_val(BaseModel):
     delete_original_after: bool
 
 class data_manag_val(BaseModel): 
-    output: output_val
     data_protection: data_protection_val
-    
     overhead_percent: float= Field(ge=1.0, le=3.0)
     sample_data_percent: float= Field(gt=0.0, lt=1.0)
 
 class data_managment_val(BaseModel): 
     path: str
-    
-    features: Optional[List[str]]
     target: str
     
     data_management: data_manag_val
