b4a5c6ad06d48e045abd5d2a49f8911b6aa233db
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-08-04 00:52:03 -0100

    null handling validation is added

--- src/validation/validation_preprocessing/preprocessing_validation.py
+++ src/validation/validation_preprocessing/preprocessing_validation.py
@@ -1,4 +1,4 @@
-from ...strategies.pre_processing_strategies import NullCategoricHandler, NullNumericHandler, DistributionTransformer, OutlierFilter, OutlierImpute, OutlierTransform, CorrSampling, HighCorrelationActions, CategoryOperation
+from ...strategies.pre_processing_strategies import NullCategoricHandler, NullNumericHandler, DistributionTransformer, OutlierFilter, OutlierImpute, OutlierTransform, CorrSampling, HighCorrelationActions, CategoryOperation, NumericNulls, CategoricNulls
 
 from ...strategies.strategies import AnalysisOutliers
 
@@ -10,25 +10,21 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(levelname)s-%(asctime)s-%(message)s')
 logger= logging.getLogger(__name__)
 
+class null_general_values_val(BaseModel): 
+    null_impute_numerics: Union[NumericNulls, int, float, None]= NumericNulls.MEDIAN
+    null_impute_categorics: Union[CategoricNulls, str, None]= CategoricNulls.MODE
+
 class distribution_val(BaseModel): 
     enable: Optional[bool]
     transformer: Optional[DistributionTransformer]
 
 class outlier_val(BaseModel): 
     enable: Optional[bool]
-    strategy: Optional[AnalysisOutliers]
+    strategy: Optional[AnalysisOutliers]='iqr'
     filter: Optional[OutlierFilter]
     impute_outliers: Optional[OutlierImpute]
     flag: Optional[bool]
     transform: Optional[OutlierTransform]
-    
-    @field_validator('strategy')
-    def strategy_val(cls, v): 
-        if not v: 
-            v= 'iqr'
-            logger.warning('As None value was given to "strategy" then the method IQR will be used')
-        
-        return v
 
 class correlation_val(BaseModel): 
     enable: Optional[bool]
@@ -73,46 +69,16 @@ class category_val(BaseModel):
 
 class ml_preprocessing_val(BaseModel): 
     representative_column: Union[str, List[str], None]
-    sampling: Optional[CorrSampling]
+    sampling: Optional[CorrSampling]= 'random'
     
-    null_num_handler: Optional[NullNumericHandler]
-    null_cat_handler: Optional[NullCategoricHandler]
-    null_cat_handler_value: Optional[str]
+    null_num_handler: Optional[NullNumericHandler]= 'median'
+    null_cat_handler: Optional[NullCategoricHandler]= 'constantValue'
+    null_cat_handler_value: Optional[str]= 'Unknown'
+    
+    null_general_values: null_general_values_val
     
     distribution: distribution_val
     outlier: outlier_val
     correlation: correlation_val
     category: category_val
-    
-    @field_validator('sampling')
-    def sampling_val(cls, v): 
-        if not v: 
-            logger.warning('As no value was found for sampling, "random" will be used')
-            return 'random'
-        else: 
-            return v
-    
-    @field_validator('null_num_handler')
-    def null_num_handler_val(cls, v): 
-        if not v: 
-            logger.warning('As no value was found for null_num_handler, "median" will be used')
-            return 'median'
-        else: 
-            return v
-    
-    @field_validator('null_cat_handler')
-    def null_cat_handler_val(cls, v): 
-        if not v: 
-            logger.warning('As no value was found for null_cat_handler, "constantValue" will be used')
-            return 'constantValue'
-        else: 
-            return v
-    
-    @field_validator('null_cat_handler_value')
-    def null_cat_handler_value_val(cls, v): 
-        if not v: 
-            logger.warning('As no value was found for null_cat_handler_value, "Unknown" will be used')
-            return 'Unknown'
-        else: 
-            return v
 
