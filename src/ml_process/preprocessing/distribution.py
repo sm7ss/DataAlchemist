956cf9fa074275ed4abdc097c5322cb3f8fb6295
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-08-06 03:49:22 -0100

    distribution was changed to fit train and transformed into test

--- src/ml_process/preprocessing/distribution.py
+++ src/ml_process/preprocessing/distribution.py
@@ -8,9 +8,10 @@ from ...strategies.pre_processing_strategies import DistributionTransformer
 logging.basicConfig(level=logging.INFO, format='%(levelname)s-%(asctime)s-%(message)s')
 logger= logging.getLogger(__name__)
 
-class DistributionListExpr: 
-    def __init__(self, frame: pl.DataFrame):
+class Distribution: 
+    def __init__(self, frame: pl.DataFrame, distribution_dict: Dict[str, Any]):
         self.num_frame= frame.select(pl.selectors.numeric())
+        self.distribution_dict= distribution_dict
     
     def transform(self, col: str, method: DistributionTransformer) -> pl.Expr: 
         transform= TransformationOperation()
@@ -25,14 +26,14 @@ class DistributionListExpr:
         
         return expr
     
-    def auto_distribution(self, distribution_dict: Dict[str, Any]) -> List[pl.Expr]: 
+    def distribution_expr(self) -> List[pl.Expr]: 
         list_expr= []
         
         for col in self.num_frame.columns: 
             if col == 'index': 
                 continue
             
-            transform= distribution_dict[col]['suggestion'].get('transformer')
+            transform= self.distribution_dict[col]['suggestion'].get('transformer')
             
             if transform: 
                 logger.info(f'The column {col} has the transformer {transform}')
@@ -42,6 +43,28 @@ class DistributionListExpr:
                 logging.info(f'Column {col} doesnt have transform suggestion')
         
         return list_expr
+    
+    def fit_expressions(self, x_train: pl.DataFrame) -> pl.DataFrame: 
+        distribution_expr= self.distribution_expr()
+        
+        if not distribution_expr: 
+            return x_train
+        
+        self.distribution= distribution_expr
+        
+        x_train= x_train.with_columns(distribution_expr)
+        logger.info('Distribution expressions were applied')
+        
+        return x_train
+    
+    def transform_expressions(self, x_test: pl.DataFrame) -> pl.DataFrame: 
+        if not self.distribution: 
+            return x_test
+        
+        x_test= x_test.with_columns(self.distribution)
+        logger.info('Distribution expressions were applied')
+        
+        return x_test
 
 
 
